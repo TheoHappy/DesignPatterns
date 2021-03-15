b12b98a397fc8dd6f2e13de59d3a3cbf62bfe21e
TheoHappy <teodore473@gmail.com>
2021-03-15 09:44:49 +0200

    Finsh composite pattern

---
+++ src/StructuralPatterns/Composite/Utilities.java
@@ -0,0 +1,10 @@
+package StructuralPatterns.Composite;
+
+public class Utilities {
+    public static String getSpaces(int number){
+        String result = "";
+        for (int i = 0; i < number; i++)
+            result = result + " ";
+        return result;
+    }
+}
--- src/StructuralPatterns/Composite2/HtmlComponent.java
+++
@@ -1,4 +0,0 @@
-package StructuralPatterns.Composite2;
-
-public class HtmlComponent {
-}
